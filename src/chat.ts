5343bd7e54a76f5cc7a2eb66d84f19ef940a9f99
Omega <web3omega@gmail.com>
2024-05-27 15:44:46 +0200

    Initial bad words filter

--- src/chat.ts
+++ src/chat.ts
@@ -1,4 +1,6 @@
 import { ChatDataMessage } from "./utils/types";
+import Filter from "bad-words";
+import badWords from "./bad-words.json";
 
 export interface VerifiedMessage {
   msg: string;
@@ -8,10 +10,25 @@ export interface VerifiedMessage {
 
 export const allChatMessages: ChatDataMessage[] = [];
 
+const filter = new Filter();
+filter.addWords(...badWords.words);
+
+const MAX_CHARS = 150;
+
 export const verifyMessage = (msg: string): VerifiedMessage => {
   //// TODO Parse the message
+  //Rule 1 Char count
+  const msgWordCounted = msg.substring(0, MAX_CHARS);
+
+  //Rule 2 regex
+  //const msgRegex = msg.replace(/[^\wèéòàùì\s]/gi, '');
+  const msgRegex = msgWordCounted.replace(/[^\x20-\x7E]/g, "?");
+
+  //Rule 3 filter bad words
+  const filteredMessage = filter.clean(msgRegex);
+
   const verifiedMessage: VerifiedMessage = {
-    msg,
+    msg: filteredMessage,
     error: false,
     errorMessage: "None",
   };
