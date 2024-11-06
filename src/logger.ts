1e8e76eace0901363b188e224dfc56b027a5dbad
Omega <web3omega@gmail.com>
2024-11-06 13:29:35 +0100

    Add logging

--- src/logger.ts
+++ src/logger.ts
@@ -3,11 +3,23 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const transport = pino.transport({
-  target: "pino-pretty",
-  options: {
-    destination: `./app.log`,
-    translateTime: "UTC:yyyy-mm-dd HH:MM:ss.l",
-  },
+  targets: [
+    {
+      target: "pino-pretty",
+      options: {
+        destination: `./app.log`,
+        translateTime: "UTC:yyyy-mm-dd HH:MM:ss.l",
+      },
+    },
+    {
+      target: "pino-axiom",
+      options: {
+        orgId: "web-omega-r4fd", // Can be found on settings page
+        token: "xaat-1f474563-86eb-4eda-ade5-0cb4f4b85612", // Can be generated on settings > API Tokens
+        dataset: "dcf_chat", // Can be created on /datasets
+      },
+    },
+  ],
 });
 
 const baseLogger = pino(
