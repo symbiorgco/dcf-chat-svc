90d1bd23f0227c8ac5ea3ea4e6f3c904f3ed3e00
Omega <web3omega@gmail.com>
2025-03-10 10:19:04 +0100

    Add reporting feature

--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -4,12 +4,14 @@ import { sendAnnouncement, viewers } from "../websockets";
 import {
   bannedUsers,
   isAdmin,
+  isAllowedToChat,
   isHelpfulDegen,
   isMod,
   recentChatMessages,
 } from "../chat";
 import { verifyJwt } from "../authentication";
-import { logger } from "../logger";
+import { logChatReport } from "../utils/modLogging";
+import { P } from "pino/pino";
 
 export const router = express.Router();
 
@@ -58,6 +60,44 @@ router.get("/get_banned_wallets", async (req, res) => {
       res.json({ error: true });
     }
   } catch (err) {
+    res.json({ error: true });
+  }
+});
+
+router.post("/report", async (req, res) => {
+  try {
+    console.log("Reporting");
+    const authKey = req.headers.authorization;
+
+    const chatProfile = await verifyJwt(authKey);
+
+    if (chatProfile) {
+      if (isAllowedToChat(chatProfile.walletId)) {
+        //Allowed to chat = allowed to report
+        const channel = Number.parseInt(req.body.channel);
+        const message = recentChatMessages
+          .get(channel)
+          .find((msg) => msg.id === req.body.id);
+        if (message) {
+          await logChatReport(
+            chatProfile.walletId,
+            chatProfile.nickname,
+            message.wallet,
+            message.username,
+            message.message
+          );
+          res.json({ completed: true });
+        } else {
+          res.json({ error: true, message: "Cannot report this message" });
+        }
+      } else {
+        res.json({ error: true, message: "Cannot report" });
+      }
+    } else {
+      res.json({ error: true });
+    }
+  } catch (err) {
+    console.log(err); //TODO REMOVE
     res.json({ error: true });
   }
 });
---
+++ src/utils/modLogging.ts
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+const BASE_URL = `https://discord.com/api/webhooks`;
+const DISCORD_CHANNELS = {
+  CHAT_MODERATION: `/1348564076275564575/L14iZ785NsnwjukKPPCcgA8PdwRWs0CBTX0XeTEygmgjdhk8sCLFNyWWZw7pzASEcetC`,
+};
+
+const logMessageToLoggerChannel = async (
+  content,
+  channel = "CHAT_MODERATION"
+) => {
+  const response = await axios.post(
+    BASE_URL + DISCORD_CHANNELS[channel],
+    content
+  );
+  return response;
+};
+
+export const logChatReport = async (
+  requestingWallet: string,
+  requestingUsername: string,
+  reportedWallet: string,
+  reportedUsername: string,
+  chatMessage: string
+) => {
+  await logMessageToLoggerChannel({
+    username: "DCF Chat Moderation",
+    embeds: [
+      {
+        title: "Chat Report",
+        color: 15258703,
+        fields: [
+          {
+            name: "Reported Message",
+            value: chatMessage,
+          },
+          {
+            name: "Wallet",
+            value: reportedWallet,
+          },
+          {
+            name: "Username",
+            value: reportedUsername,
+          },
+        ],
+        footer: {
+          text: `Reported by ${requestingWallet} - ${requestingUsername} `,
+        },
+      },
+    ],
+  });
+};
